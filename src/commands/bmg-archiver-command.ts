699bd3c47c712c097e9590ece357944b9b516716
badre429 <badre429@hotmail.com>
2020-09-27 22:19:47 +0100

    migrate

--- src/commands/bmg-archiver-command.ts
+++ src/commands/bmg-archiver-command.ts
@@ -3,11 +3,16 @@
 
 import { FilesIgnoreHelper } from '../utils/files-helpers';
 import * as path from 'path';
-import * as fs from 'fs';
-import * as archiver from 'archiver';
+// import * as fs from 'fs';
+// import * as archiver from 'archiver';
+const zl = require('zip-lib');
+// import * as zl from 'zip-lib';
 import { BmgCommandHelper } from './bmg-command-helper';
 import * as vscode from 'vscode';
 import { BmgCommand } from './bmg-command';
+import { execSync } from 'child_process';
+// const { exec, execSync } = require('child_process');
+
 export class BmgArchiverCommand extends BmgCommand {
   constructor(private context: vscode.ExtensionContext) {
     super(context, 'extension.bmg.archiver');
@@ -15,47 +20,87 @@ export class BmgArchiverCommand extends BmgCommand {
   Run() {
     if (vscode.workspace.rootPath != null) {
       BmgArchiverCommand.CreateArchive(vscode.workspace.rootPath);
-    } else
+    } else {
       vscode.window.showInformationMessage(
         'You have to open a folder to run this command'
       );
+    }
   }
   public static CreateArchive(directoryPath = null) {
     var startTime = new Date();
     directoryPath = directoryPath == null ? __dirname : directoryPath;
-    var ih = new FilesIgnoreHelper(directoryPath);
-    var allIncludedFiles = ih.GetGitFilteredFiles();
-
+    var allIncludedFiles = [];
+    try {
+      var stdout = [
+        ...execSync('git ls-files', {
+          cwd: directoryPath,
+        })
+          .toString()
+          .split('\n'),
+        ...execSync('git ls-files -o  --exclude-standard', {
+          cwd: directoryPath,
+        })
+          .toString()
+          .split('\n'),
+      ];
+      allIncludedFiles = stdout;
+    } catch (error) {}
+    if (allIncludedFiles.length == 0) {
+      var ih = new FilesIgnoreHelper(directoryPath);
+      allIncludedFiles = ih.GetGitFilteredFiles();
+    }
+    // the *entire* stdout and stderr (buffered)
+    vscode.window.showInformationMessage(
+      'start compression of ' + allIncludedFiles.length + ' files'
+    );
     var zippath = path.join(
       path.join(directoryPath, '..'),
       path.basename(directoryPath) +
         '_' +
-        startTime
-          .toISOString()
-          .replace(':', '-')
-          .replace(':', '-') +
+        startTime.toISOString().replace(':', '-').replace(':', '-') +
         '.zip'
     );
 
-    var output = fs.createWriteStream(zippath);
-    var archive = (<any>archiver)('zip', {
-      zlib: {
-        level: 9
-      } // Sets the compression level.
-    });
+    const zip = new zl.Zip();
+    // Adds a file from the file system
 
-    archive.pipe(output);
-    allIncludedFiles.forEach(function(file) {
+    allIncludedFiles.forEach((file) => {
       var abs_file = path.join(directoryPath, file);
 
-      // add local file
-
-      archive.append(fs.createReadStream(abs_file), {
-        name: path.join(path.basename(directoryPath), file)
-      });
+      zip.addFile(abs_file, path.join(path.basename(directoryPath), file));
     });
+    // Adds a folder from the file system, putting its contents at the root of archive
 
-    archive.finalize();
-    BmgCommandHelper.OpenInExplorer(zippath);
+    // Generate zip file.
+    zip.archive(zippath).then(
+      function () {
+        console.log('done');
+        BmgCommandHelper.OpenInExplorer(zippath);
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+
+    // var output = fs.createWriteStream(zippath);
+    // var archive = (<any>archiver)('zip', {
+    //   zlib: {
+    //     level: 9,
+    //   }, // Sets the compression level.
+    // });
+    // output.on('close', function () {
+    //   BmgCommandHelper.OpenInExplorer(zippath);
+    // });
+
+    // archive.pipe(output);
+    // allIncludedFiles.forEach(function (file) {
+    //   var abs_file = path.join(directoryPath, file);
+
+    //   archive.file(abs_file, {
+    //     name: path.join(path.basename(directoryPath), file),
+    //   });
+    // });
+
+    // archive.finalize();
   }
 }
