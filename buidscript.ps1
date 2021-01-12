21430ca1db8423d6c534cce32d6aa7062db96da8
badre429 <badre429@hotmail.com>
2021-01-12 23:25:09 +0100

    ci build remove eot files

--- buidscript.ps1
+++ buidscript.ps1
@@ -1,10 +1,18 @@
+yarn
 Remove-Item -Recurse -Force .\media\external\
 New-Item -ItemType Directory .\media\external\
 
-Copy-Item .\node_modules\vuetify\dist\vuetify.css .\media\external\
-Copy-Item .\node_modules\vuetify\dist\vuetify.js .\media\external\
-Copy-Item .\node_modules\vue\dist\vue.js .\media\external\
+Copy-Item .\node_modules\vuetify\dist\vuetify.min.css .\media\external\vuetify.css
+Copy-Item .\node_modules\vuetify\dist\vuetify.min.js .\media\external\vuetify.js
+Copy-Item .\node_modules\vue\dist\vue.min.js .\media\external\vue.js
+
 
 Copy-Item -Recurse .\node_modules\material-design-icons-iconfont\dist\* .\media\external\
-
- 
+Remove-Item -Force .\media\*.eot
+Get-ChildItem -Force -Recurse .\media\**\*.eot | Remove-Item 
+Get-ChildItem -Force -Recurse .\media\**\*.ttf | Remove-Item 
+Get-ChildItem -Force -Recurse .\media\**\*.woff | Remove-Item 
+Get-ChildItem -Force -Recurse .\media\external\**\*.map | Remove-Item 
+Get-ChildItem -Force -Recurse .\media\external\**\*.json | Remove-Item 
+Get-ChildItem -Force -Recurse .\media\**\LICENSE | Remove-Item 
+npm run build 
