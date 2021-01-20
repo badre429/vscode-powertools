7f58b499f8e23c0a4b6c4da110acecd8295f26c0
badre429@hotmail.com <badre429@hotmail.com>
2021-01-20 13:25:16 +0100

    feat:add encode JSON Multiline snippet

--- src/commands/bmg-encode-command.ts
+++ src/commands/bmg-encode-command.ts
@@ -20,8 +20,9 @@ export class BmgEncodeCommand extends BmgCommand {
         'HTML Non UTF',
         'HTML Non ASCII',
         'JSON Multiline code',
+        'JSON Multiline snippet',
         'latin1',
-         'hex',
+        'hex',
         'base64',
       ]),
     });
@@ -74,6 +75,13 @@ export class BmgEncodeCommand extends BmgCommand {
           .join('+\n');
 
         break;
+      case 'JSON Multiline snippet':
+        hashText = selectedText
+          .split('\n')
+          .map((e) => JSON.stringify(e))
+          .join(',\n');
+
+        break;
       default:
         hashText = selectedText;
         break;
@@ -81,32 +89,3 @@ export class BmgEncodeCommand extends BmgCommand {
     BmgCommandHelper.ReplaceEditorSelection(hashText);
   }
 }
-
-// XML entities
-
-// HTML validity and XSS attack prevention you can achieve from XmlEntities class.
-
-// const Entities = require('html-entities').XmlEntities;
-
-// const entities = new Entities();
-
-// console.log(entities.encode('<>"\'&©®')); // &lt;&gt;&quot;&apos;&amp;©®
-// console.log(entities.encodeNonUTF('<>"\'&©®')); // &lt;&gt;&quot;&apos;&amp;&#169;&#174;
-// console.log(entities.encodeNonASCII('<>"\'&©®')); // <>"\'&©®
-// console.log(entities.decode('&lt;&gt;&quot;&apos;&amp;&copy;&reg;&#8710;')); // <>"'&&copy;&reg;∆
-// All HTML entities encoding/decoding
-
-// const Entities = require('html-entities').AllHtmlEntities;
-
-// const entities = new Entities();
-
-// console.log(entities.encode('<>"&©®∆')); // &lt;&gt;&quot;&amp;&copy;&reg;∆
-// console.log(entities.encodeNonUTF('<>"&©®∆')); // &lt;&gt;&quot;&amp;&copy;&reg;&#8710;
-// console.log(entities.encodeNonASCII('<>"&©®∆')); // <>"&©®&#8710;
-// console.log(entities.decode('&lt;&gt;&quot;&amp;&copy;&reg;')); // <>"&©®
-// Available classes
-
-// const XmlEntities = require('html-entities').XmlEntities, // <>"'& + &#...; decoding
-//       Html4Entities = require('html-entities').Html4Entities, // HTML4 entities.
-//       Html5Entities = require('html-entities').Html5Entities, // HTML5 entities.
-//       AllHtmlEntities = require('html-entities').AllHtmlEntities
